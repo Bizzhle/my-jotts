1e725acbd324f4d69cfd320150a6c3a91ab49da1
Paul Egbo <gabrieluche2@gmail.com>
2023-06-16 23:19:43 +0200

    initial backend setup with typeorm, postgress and docker

--- portal-backend/src/main.ts
+++ portal-backend/src/main.ts
@@ -1,16 +1,13 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('Portal Backend Api')
-    .setVersion('1.0')
-    .build();
+  const config = new DocumentBuilder().setTitle('Portal Backend Api').setVersion('1.0').build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
