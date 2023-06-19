2ddeefea58f3fd3bbc63e7a5905217632a5b359a
Paul Egbo <gabrieluche2@gmail.com>
2023-06-20 00:47:14 +0200

    portal-backend: resources and schema for food, user, recipe aand restauarant created

---
+++ portal-backend/src/users/entities/user.entity.ts
@@ -0,0 +1,38 @@
+import { UUID } from 'crypto';
+import { Food } from '../../food/entities/food.entity';
+import { Recipe } from '../../recipe/entities/recipe.entity';
+import { Restaurant } from '../../restaurant/entities/restaurant.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'user_account' })
+export class UserAccount {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @PrimaryGeneratedColumn('uuid')
+  userId: string;
+
+  @Column()
+  emailAddress: string;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  enabled: boolean;
+
+  @Column()
+  registrationDate: Date;
+
+  @OneToMany(() => Restaurant, (restaurant) => restaurant.userAccount)
+  restaurants: Restaurant[];
+
+  @OneToMany(() => Recipe, (recipe) => recipe.userAccount)
+  recipes: Recipe[];
+
+  @OneToMany(() => Food, (food) => food.userAccount)
+  foods: Food[];
+}
