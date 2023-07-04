1c063e95fc729aaa3312a5bbcae59f1bbbde0915
Paul Egbo <gabrieluche2@gmail.com>
2023-07-04 22:59:40 +0200

    portal-backend: jwt strategy and guard added

--- portal-backend/src/users/guards/jwt.auth.guard.ts
+++ portal-backend/src/users/guards/jwt.auth.guard.ts
@@ -1,15 +1,27 @@
 import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { Observable } from 'rxjs';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-  // add custom authentication logic here
-  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-    // return super.canActivate(context);
-    const request = context.switchToHttp().getRequest();
-    return request.isAuthenticated();
+  canActivate(context: ExecutionContext) {
+    // Add your custom authentication logic here
+    // for example, call super.logIn(request) to establish a session.
+    return super.canActivate(context);
   }
+
+  handleRequest(err, user, info) {
+    // You can throw an exception based on either "info" or "err" arguments
+    if (err || !user) {
+      throw err || new UnauthorizedException(' cant');
+    }
+    return user;
+  }
+  // add custom authentication logic here
+  // canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+  //   return super.canActivate(context);
+  //   // const request = context.switchToHttp().getRequest();
+  //   // return request.isAuthenticated();
+  // }
   // handleRequest<TUser = any>(
   //   err: any,
   //   user: any,
