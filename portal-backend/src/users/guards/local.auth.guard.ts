9fb87f2b85819988d168384592b0a5791b29d010
Paul Egbo <gabrieluche2@gmail.com>
2024-03-03 13:04:21 +0100

    backend-multer: authentication updated

--- portal-backend/src/users/guards/local.auth.guard.ts
+++ portal-backend/src/users/guards/local.auth.guard.ts
@@ -1,5 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class LocalAuthGuard extends AuthGuard('local') {}
+
+export function HasAccess() {
+  return UseGuards(LocalAuthGuard);
+}
