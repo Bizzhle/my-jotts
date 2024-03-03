c86d68b3592e024bf628f0700199771b75ce5ea4
Paul Egbo <gabrieluche2@gmail.com>
2024-03-03 12:52:02 +0100

    backend-multer: deleted  or updated files

--- portal-backend/src/main.ts
+++ portal-backend/src/main.ts
@@ -1,7 +1,9 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './app/exceptions/http-exception-filter';
+import { CustomLogger } from './logs/services/customLogger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -12,6 +14,8 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({ stopAtFirstError: true, forbidNonWhitelisted: true, transform: true }),
   );
+  app.useLogger(new CustomLogger());
+  app.useGlobalFilters(new HttpExceptionFilter());
   const config = new DocumentBuilder().setTitle('Portal Backend Api').setVersion('1.0').build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
