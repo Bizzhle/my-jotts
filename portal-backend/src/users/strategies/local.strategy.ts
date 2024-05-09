ff977af52804e5b0d1fe4922648629f1555b5c82
Paul Egbo <gabrieluche2@gmail.com>
2024-05-09 20:20:53 +0200

    backend: authentication with little tweaks

--- portal-backend/src/users/strategies/local.strategy.ts
+++ portal-backend/src/users/strategies/local.strategy.ts
@@ -1,7 +1,7 @@
-import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { AuthService } from '../services/user-auth/auth.services';
+import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
+import { AuthService } from '../services/user-auth/auth.services';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
@@ -15,7 +15,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   async validate(emailAddress: string, password: string): Promise<any> {
     const user = await this.authService.validateUser(emailAddress, password);
     if (!user) {
-      throw new UnauthorizedException('here');
+      throw new UnauthorizedException();
     }
     return user;
   }
