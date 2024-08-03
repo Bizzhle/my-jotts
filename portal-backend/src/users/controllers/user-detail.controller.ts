d5e8dd41af864230a9b0ef68df9b0ba1d0af8ea0
Paul Egbo <gabrieluche2@gmail.com>
2024-08-03 17:03:56 +0200

    backend: swagger updated

--- portal-backend/src/users/controllers/user-detail.controller.ts
+++ portal-backend/src/users/controllers/user-detail.controller.ts
@@ -1,5 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiInternalServerErrorResponse,
+  ApiOperation,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 import { GetCurrentUserFromJwt } from '../../app/jwt.decorators';
 import { UserAccount } from '../entities/user-account.entity';
 import { IsAuthenticatedUser } from '../guards/jwt.auth.guard';
@@ -12,6 +18,11 @@ export class UserDetailController {
 
   @IsAuthenticatedUser()
   @Get('me')
+  @ApiOperation({ description: 'return user"s details' })
+  @HttpCode(HttpStatus.OK)
+  @ApiBadRequestResponse({ description: 'invalid credential' })
+  @ApiUnauthorizedResponse({ description: 'invalid credentials' })
+  @ApiInternalServerErrorResponse({ description: 'server error' })
   async getUser(@GetCurrentUserFromJwt() emailAddress: string): Promise<UserAccount> {
     return await this.userDetailService.getUserProfileByEmail(emailAddress);
   }
