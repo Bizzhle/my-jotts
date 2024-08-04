01c5229dfa6e08eab6f5f554957daff8762095b8
Paul Egbo <gabrieluche2@gmail.com>
2024-08-04 18:11:15 +0200

    frontend: pages created

---
+++ frontend/src/app/styles/theme.tsx
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#2c5c51",
+      light: "",
+      dark: "",
+    },
+    secondary: {
+      main: "#FF5733",
+    },
+  },
+});
--- frontend/src/app/webapp/pages/HomePage.tsx
+++ frontend/src/app/webapp/pages/HomePage.tsx
@@ -1,5 +1,14 @@
 import { Typography } from "@mui/material";
+import { AuthContext } from "../utils/contexts/AuthContext";
+import { useContext } from "react";
 
 export default function HomePage() {
-  return <Typography>HomePage</Typography>;
+  const { authenticatedUser } = useContext(AuthContext);
+
+  console.log(authenticatedUser);
+  return (
+    <Typography>
+      HomePageggsgsgsgsgsahsgagsgagagagaggag sjhasasgggsggsgs
+    </Typography>
+  );
 }
