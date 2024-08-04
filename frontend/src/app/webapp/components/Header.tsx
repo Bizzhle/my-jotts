cfe2bd72c46cf77ed08a2802ee747ba1bf645c98
Paul Egbo <gabrieluche2@gmail.com>
2024-08-04 18:11:15 +0200

    frontend: pages created

--- frontend/src/app/webapp/components/Header.tsx
+++ frontend/src/app/webapp/components/Header.tsx
@@ -1,37 +1,177 @@
 import {
   AppBar,
+  Box,
+  Button,
   Container,
   IconButton,
+  Menu,
+  MenuItem,
+  Modal,
   Toolbar,
   Typography,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
+import { useContext, useState } from "react";
+import SearchBar from "./SearchBar";
+import { AccountCircle, MoreVert } from "@mui/icons-material";
+import { AuthContext } from "../utils/contexts/AuthContext";
 
 export default function Header() {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
+    useState<null | HTMLElement>(null);
+  const [showMenuIcon, setShowMenuIcon] = useState<Boolean>(false);
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+  const { logoutUser } = useContext(AuthContext);
+
+  const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const handleLogout = async () => {
+    await logoutUser();
+  };
+
+  const menuId = "primary-search-account-menu";
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = "primary-search-account-menu-mobile";
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
+
   return (
     <>
       <AppBar
         position="fixed"
         sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        color="primary"
       >
         <Container maxWidth="xl">
           <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 1 }}
+            {showMenuIcon && (
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 1 }}
+              >
+                <MenuIcon />
+              </IconButton>
+            )}
+            <Typography
+              component="a"
+              href="/"
+              variant="h6"
+              noWrap
+              sx={{ display: { xs: "none", sm: "block" }, color: "inherit" }}
             >
-              <MenuIcon />
-            </IconButton>
-
-            <Typography component="a" href="/" sx={{ color: "inherit" }}>
-              LOGO
+              MyJotta
             </Typography>
+            <SearchBar />
+            <Box sx={{ flexGrow: 1 }} />
+            <Typography component="a" href="#">
+              Dashboard
+            </Typography>
+            <Button
+              variant="outlined"
+              sx={{ display: { xs: "none", md: "flex" }, color: "inherit" }}
+              onClick={handleLogout}
+            >
+              Logout
+            </Button>
+
+            {/* <Box sx={{ display: { xs: "none", md: "flex" } }}>
+              <IconButton
+                size="large"
+                edge="end"
+                aria-label="account of current user"
+                aria-controls={menuId}
+                aria-haspopup="true"
+                onClick={handleProfileMenuOpen}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+            </Box>
+            <Box sx={{ display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="show more"
+                aria-controls={mobileMenuId}
+                aria-haspopup="true"
+                onClick={handleMobileMenuOpen}
+                color="inherit"
+              >
+                <MoreVert />
+              </IconButton>
+            </Box> */}
           </Toolbar>
         </Container>
       </AppBar>
+      {/* {renderMobileMenu}
+      {renderMenu} */}
     </>
   );
 }
