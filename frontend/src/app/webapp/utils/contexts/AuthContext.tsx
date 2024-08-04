681eab2c188e4960962e916c722489f6221bd52c
Paul Egbo <gabrieluche2@gmail.com>
2024-08-04 18:12:05 +0200

    frontend: context  api added

---
+++ frontend/src/app/webapp/utils/contexts/AuthContext.tsx
@@ -0,0 +1,98 @@
+import { createContext, useContext, useEffect } from "react";
+import { useObjectReducer } from "../shared/objectReducer";
+import {
+  getUserData,
+  logout,
+} from "../../../api-service/services/auth-service";
+import { SessionState } from "../../../libs/SessionState";
+
+interface AuthUserProviderProps {
+  children?: React.ReactElement;
+}
+export interface UserInfo {
+  id: number;
+  emailAddress: string;
+  firstName: string;
+  lastName: string;
+  enabled: boolean;
+  registrationDate: Date | string;
+  lastLoggedIn: Date | string;
+}
+
+export interface SessionInfo {
+  emailAddress: string;
+}
+
+export interface AuthUserContextState {
+  readonly authenticatedUser?: UserInfo;
+  readonly sessionInfo?: SessionInfo;
+}
+
+export interface AuthContextValue extends AuthUserContextState {
+  readonly isAuthenticated: boolean;
+  readonly isLoggedIn: boolean;
+  getUserInfo: () => void;
+  logoutUser: () => void;
+  startSession: (data: SessionInfo) => void;
+}
+
+export const AuthContext = createContext<AuthContextValue>({} as never);
+
+export interface useAuthenticatedUserReturn extends AuthContextValue {
+  authenticatedUser?: UserInfo;
+}
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+
+  if (!context.authenticatedUser) {
+    throw new Error();
+  }
+
+  return context;
+};
+
+export function AuthProvider(props: AuthUserProviderProps) {
+  const [state, setState] = useObjectReducer<AuthUserContextState>({});
+
+  async function getUserInfo() {
+    const user = await getUserData();
+    setState("authenticatedUser", user);
+  }
+
+  async function startSession(data: SessionInfo) {
+    setState("sessionInfo", data);
+  }
+
+  async function logoutUser() {
+    const refreshToken = SessionState.refreshToken;
+    if (refreshToken) {
+      await logout({ refreshToken: refreshToken });
+      setState("authenticatedUser", undefined);
+      SessionState.removeAccessToken();
+      SessionState.removeRefreshToken();
+    }
+  }
+
+  console.log(state.authenticatedUser);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        getUserInfo,
+        logoutUser,
+        startSession,
+        get isLoggedIn() {
+          return !!state.authenticatedUser;
+        },
+        get isAuthenticated() {
+          return !!state.sessionInfo;
+        },
+        get authenticatedUser() {
+          return state.authenticatedUser;
+        },
+      }}
+      {...props}
+    />
+  );
+}
---
+++ frontend/src/app/webapp/utils/shared/objectReducer.ts
@@ -0,0 +1,49 @@
+import { Reducer, useCallback, useReducer } from "react";
+
+type ReducerState = Record<string, unknown>;
+type ReducerAction = { key: keyof ReducerState; value: unknown };
+
+const objectReducer: Reducer<ReducerState, ReducerAction> = function (
+  state,
+  { key, value }
+) {
+  return { ...state, [key]: value };
+};
+
+export type setStateFn<State> = {
+  <Key extends keyof State>(key: Key, value: State[Key]): void;
+  (partial: Partial<State>): void;
+};
+// function reducer<State, Action>(state: State, action:Action) {
+//   switch (action.type) {
+//     case "UPDATE_VALUE":
+//       return {
+//         ...state,
+//         [action.key]: action.value,
+//       };
+
+//     case "RESET":
+//       return action.initialState;
+
+//     default: {
+//       throw new Error();
+//     }
+//   }
+// }
+export function useObjectReducer<T>(initialState: T) {
+  const [state, dispatch] = useReducer(
+    objectReducer as Reducer<T, ReducerAction>,
+    initialState
+  );
+
+  const setState: setStateFn<T> = useCallback(
+    (key: string | number | symbol | Partial<T>, value?: unknown) => {
+      if (typeof key === "object")
+        Object.entries(key).forEach(([key, value]) => dispatch({ key, value }));
+      dispatch({ key, value } as ReducerAction);
+    },
+    []
+  );
+
+  return [state, setState] as const;
+}
