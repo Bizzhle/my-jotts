dad88ebe850bf7f07b93bf3ba1ba2287d101963b
Paul Egbo <gabrieluche2@gmail.com>
2024-08-09 23:36:47 +0200

    backend: remove redundant components

--- portal-backend/src/users/dto/post-login-response.dto.ts
+++ portal-backend/src/users/dto/post-login-response.dto.ts
@@ -3,3 +3,7 @@ export class PostLoginResponseDTO {
   accessToken: string;
   refreshToken: string;
 }
+
+export class PostLoginDTO {
+  refreshToken: string;
+}
--- portal-backend/src/users/dto/update-user.dto.ts
+++ portal-backend/src/users/dto/update-user.dto.ts
@@ -1,6 +1,16 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, Validate } from 'class-validator';
+import { UserPasswordValidator } from '../types/validators/user-password.validators';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
+export class UpdateUserDto {
   id: number;
+
+  @ApiProperty({ description: 'Email Address', example: 'test@gmail.com' })
+  @IsEmail()
+  emailAddress: string;
+
+  @ApiProperty({ description: 'password of user', example: 'Pass12345' })
+  @Validate(UserPasswordValidator)
+  @IsNotEmpty()
+  password: string;
 }
