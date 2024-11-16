514f44543977e7066cfde3c73bef1e16047f18d1
Paul Egbo <gabrieluche2@gmail.com>
2024-11-16 23:10:51 +0100

    frontend: url paths renamed

--- frontend/src/app/libs/auth/RequireAuth.tsx
+++ frontend/src/app/libs/auth/RequireAuth.tsx
@@ -2,17 +2,6 @@ import { useLocation, Navigate } from "react-router-dom";
 import { ReactNode, useContext } from "react";
 import { AuthContext } from "../../webapp/utils/contexts/AuthContext";
 
-// export function RequireAuth({ children }: { children: JSX.Element }) {
-//   const { authenticatedUser } = useAuthUser();
-//   const location = useLocation();
-
-//   if (!authenticatedUser) {
-//     return <Navigate to="/login" state={{ from: location }} replace />;
-//   }
-
-//   return children;
-// }
-
 interface ProtectedLoaderProps {
   children: ReactNode;
 }
