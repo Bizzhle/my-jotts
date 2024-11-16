11a23a6424ad99662eb6b044efd12c184536da45
Paul Egbo <gabrieluche2@gmail.com>
2024-11-17 00:01:30 +0100

    backend: tests updated

--- portal-backend/src/category/services/category.service.spec.ts
+++ portal-backend/src/category/services/category.service.spec.ts
@@ -1,18 +1,122 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoryService } from './category.service';
+import { AppLoggerService } from '../../logger/services/app-logger.service';
+import { UsersService } from '../../users/services/user-service/users.service';
+import { ActivityRepository } from '../../activity/repositories/activity.repository';
+import { CategoryRepository } from '../repositories/category.repository';
+import { moveCursor } from 'readline';
+import { result } from 'lodash';
 
 describe('CategoryService', () => {
   let service: CategoryService;
+  let categoryRepository;
+  let usersService;
+
+  const mockUser = {
+    id: 1,
+    email_address: 'email@email.com',
+    first_name: 'email',
+    last_name: 'test',
+    registration_date: new Date(),
+    last_logged_in: new Date(),
+  };
+
+  const category = {
+    id: 1,
+    category_name: 'test category',
+    user_id: mockUser.id,
+    description: 'Test',
+    userAccount: null,
+    activities: null,
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CategoryService],
+      providers: [
+        CategoryService,
+        {
+          provide: CategoryRepository,
+          useValue: {
+            findCategoryById: jest.fn(),
+            findCategoryByName: jest.fn(),
+            findAllCategoriesForUser: jest.fn(),
+            createCategory: jest.fn(),
+            updateCategory: jest.fn(),
+            deleteCategory: jest.fn(),
+          },
+        },
+        {
+          provide: AppLoggerService,
+          useValue: {
+            debug: jest.fn(),
+          },
+        },
+        {
+          provide: UsersService,
+          useValue: {
+            getUserByEmail: jest.fn(),
+          },
+        },
+        {
+          provide: ActivityRepository,
+          useValue: {
+            count: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<CategoryService>(CategoryService);
+    categoryRepository = module.get<CategoryRepository>(CategoryRepository);
+    usersService = module.get<UsersService>(UsersService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('creates a category', async () => {
+    jest.spyOn(usersService, 'getUserByEmail').mockResolvedValue(mockUser);
+    jest.spyOn(service, 'createCategory').mockResolvedValue(category);
+
+    const createDto = {
+      categoryName: 'test category',
+      description: 'Test',
+    };
+    const result = await service.createCategory(createDto, mockUser.email_address);
+    expect(result).toEqual(category);
+  });
+
+  it('returns all user categories related to user', async () => {
+    jest.spyOn(usersService, 'getUserByEmail').mockResolvedValue(mockUser);
+    jest.spyOn(service, 'getAllUserCategories').mockResolvedValue([category]);
+
+    const res = await service.getAllUserCategories(mockUser.email_address);
+    expect(res).toEqual([category]);
+  });
+
+  it('updates a category', async () => {
+    jest.spyOn(usersService, 'getUserByEmail').mockResolvedValue(mockUser);
+    jest
+      .spyOn(categoryRepository, 'updateCategory')
+      .mockResolvedValue({ ...category, category_name: 'updated category' });
+
+    const updateDto = {
+      categoryName: 'test category',
+      description: 'Test',
+    };
+    const result = await service.updateCategory(category.id, updateDto, mockUser.email_address);
+    expect(result).toEqual({
+      id: 1,
+      category_name: 'updated category',
+      user_id: mockUser.id,
+      description: 'Test',
+      userAccount: null,
+      activities: null,
+    });
+  });
+
+  it('deletes an activity', async () => {
+    jest.spyOn(usersService, 'getUserByEmail').mockResolvedValue(mockUser);
+    jest.spyOn(service, 'deleteCategory').mockResolvedValue();
+
+    const res = await service.deleteCategory(category.id);
+    expect(res).toEqual(undefined);
   });
 });
--- portal-backend/src/users/services/__tests__/users.service.spec.ts
+++ portal-backend/src/users/services/__tests__/users.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from '../user-service/users.service';
+import { UserAccountRepository } from '../../repositories/user-account.repository';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: UserAccountRepository,
+          useValue: {
+            findOne: jest.fn(),
+            findUserByEmail: jest.fn(),
+            findUserById: jest.fn(),
+            getUserDetail: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
