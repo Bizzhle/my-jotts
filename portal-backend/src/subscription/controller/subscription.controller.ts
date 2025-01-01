31d22e840dfa575f2897822fd81e5ba920799084
Paul Egbo <gabrieluche2@gmail.com>
2025-01-01 12:06:20 +0100

    backend: logic to store subscription, invoice and payment plan

---
+++ portal-backend/src/subscription/controller/subscription.controller.ts
@@ -0,0 +1,78 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Req,
+  Res,
+} from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiInternalServerErrorResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { GetCurrentUserFromJwt } from '../../app/jwt.decorators';
+import { CreateSubscriptionDto } from '../dtos/create-subscription.dto';
+import { SubscriptionService } from '../services/subscription.service';
+import { Request, Response } from 'express';
+import { IsAuthorizedUser } from '../../auth/guards/auth.guard';
+
+@ApiTags('Subscription')
+@Controller('subscription')
+export class SubscriptionController {
+  constructor(private readonly subscriptionService: SubscriptionService) {}
+
+  @Get()
+  @IsAuthorizedUser()
+  @ApiBearerAuth()
+  @ApiOperation({
+    description: 'Returns user subscription details',
+  })
+  @ApiOkResponse({ status: 201, description: 'Subscription information were returned' })
+  @ApiNotFoundResponse({ description: 'User not found' })
+  @ApiUnauthorizedResponse({ description: 'User not logged in or invalid credentials' })
+  @ApiInternalServerErrorResponse({ description: 'Server unavailable' })
+  async getSubscription(@GetCurrentUserFromJwt() username: string) {
+    return await this.subscriptionService.getUserSubscriptionInformation(username);
+  }
+
+  @Post()
+  @IsAuthorizedUser()
+  @ApiBearerAuth()
+  @ApiOperation({ description: 'Create a subscription for user' })
+  @HttpCode(HttpStatus.CREATED)
+  @ApiOkResponse({ status: 201, description: 'Subscription is created' })
+  @ApiInternalServerErrorResponse({ description: 'Server unavailable' })
+  async createSubscription(
+    @Body() dto: CreateSubscriptionDto,
+    @GetCurrentUserFromJwt() username: string,
+  ) {
+    return this.subscriptionService.createSubscription(dto, username);
+  }
+
+  @Delete(':id')
+  @IsAuthorizedUser()
+  @ApiBearerAuth()
+  async cancelSubscription(@Param('id') id: string) {
+    return this.subscriptionService.cancelSubscription(id);
+  }
+
+  @Post('webhook')
+  async webhook(@Req() req: Request, @Res() res: Response) {
+    try {
+      await this.subscriptionService.webhook(req, res);
+      res.status(HttpStatus.OK).send();
+    } catch (error) {
+      console.error('Webhook error:', error);
+      res.status(HttpStatus.INTERNAL_SERVER_ERROR).send('Webhook processing failed');
+    }
+  }
+}
