45dfd273754d6d938e15d83fe844e08db603210c
Paul Egbo <gabrieluche2@gmail.com>
2025-04-22 19:22:53 +0200

    semantic changes

--- portal-backend/src/auth/auth.module.ts
+++ portal-backend/src/auth/auth.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './controller/auth.controller';
-import { UserAuthService } from './services/userauth.service';
-import { PasswordService } from './services/password.service';
-import { UsersModule } from '../users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { PaswordResetToken } from '../users/entities/password-reset-token.entity';
+import { PasswordResetToken } from '../users/entities/password-reset-token.entity';
+import { UsersModule } from '../users/users.module';
 import { UtilsModule } from '../utils/util.module';
+import { AuthController } from './controller/auth.controller';
 import { UsersRegistrationController } from './controller/users-registration.controller';
+import { PasswordService } from './services/password.service';
 import { UserRegistrationService } from './services/user-registration.service';
+import { UserAuthService } from './services/userauth.service';
 
 @Module({
-  imports: [UsersModule, TypeOrmModule.forFeature([PaswordResetToken]), UtilsModule],
+  imports: [UsersModule, TypeOrmModule.forFeature([PasswordResetToken]), UtilsModule],
   controllers: [AuthController, UsersRegistrationController],
   providers: [UserAuthService, PasswordService, UserRegistrationService],
 })
