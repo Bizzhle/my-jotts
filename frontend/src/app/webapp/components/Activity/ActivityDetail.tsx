58447b7ad06e1ccfb2dc45fa86de8adafd6c60fe
Paul Egbo <gabrieluche2@gmail.com>
2025-05-11 15:22:41 +0200

    update layout for some components

--- frontend/src/app/webapp/components/Activity/ActivityDetail.tsx
+++ frontend/src/app/webapp/components/Activity/ActivityDetail.tsx
@@ -5,22 +5,17 @@ import {
   Container,
   Paper,
   Rating,
-  Toolbar,
   Typography,
-  useMediaQuery,
-  useTheme,
 } from "@mui/material";
 import { useContext, useEffect, useState } from "react";
 import Carousel from "react-material-ui-carousel";
 import { Link } from "react-router-dom";
 import { LayoutContext } from "../../layout/LayoutContext";
-import { useActivities } from "../../utils/contexts/ActivityContext";
+import { useActivities } from "../../utils/contexts/hooks/useActivities";
 import useS3Image from "../../utils/hooks/useS3Image";
 import ActivityDialogForm from "./ActivityDialogForm";
 
 export default function ActivityDetail() {
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
   const { activityData } = useActivities();
   const [activityFormOpen, setActivityFormOpen] = useState<boolean>(false);
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -40,14 +35,7 @@ export default function ActivityDetail() {
 
   return (
     <Container maxWidth="sm">
-      {!isMobile && <Toolbar />}
-      <Box>
-        <Typography variant="h5" sx={{ mb: 2 }}>
-          Activity
-        </Typography>
-      </Box>
-
-      <Box mb={2}>
+      <Box sx={{ mb: 2, py: 2 }}>
         <Breadcrumbs aria-label="breadcrumb">
           <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
             Activities
