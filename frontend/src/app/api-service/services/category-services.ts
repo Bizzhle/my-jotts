d0b7273c38122dc2e1b6c18a9164d5a54bb04b30
Paul Egbo <gabrieluche2@gmail.com>
2025-05-11 21:53:30 +0200

    remove deprecated api request services

--- frontend/src/app/api-service/services/category-services.ts
+++
@@ -1,6 +0,0 @@
-import apiClient from "../../libs/Configs/axiosConfig";
-import { CategoryData } from "../dtos/category.dto";
-
-export async function createCategory(dto: CategoryData) {
-  return await apiClient.post(`/category`, dto);
-}
