a9d318d8ee19209d411d607df8ded6847c90e2ba
Paul Egbo <gabrieluche2@gmail.com>
2025-10-20 00:40:50 +0200

    intiial authguard and subscription service

--- portal-backend/src/auth/guards/auth.guard.ts
+++ portal-backend/src/auth/guards/auth.guard.ts
@@ -1,41 +1,33 @@
-import {
-  CanActivate,
-  ExecutionContext,
-  Injectable,
-  UnauthorizedException,
-  UseGuards,
-} from '@nestjs/common';
-import { Request } from 'express';
-import { RequestContext } from '../../app/middleware/request.context.interface';
-import { JwtSigningService } from '../../utils/services/jwt-signing.services';
+import { UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@thallesp/nestjs-better-auth';
 
-@Injectable()
-export class AuthGuard implements CanActivate {
-  constructor(private readonly jwtSigningService: JwtSigningService) {}
-  async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request: RequestContext = context.switchToHttp().getRequest();
-    const token = await this.validateRequest(request);
+// @Injectable()
+// export class AuthGuard implements CanActivate {
+//   constructor(private readonly jwtSigningService: JwtSigningService) {}
+//   async canActivate(context: ExecutionContext): Promise<boolean> {
+//     const request: RequestContext = context.switchToHttp().getRequest();
+//     const token = await this.validateRequest(request);
 
-    try {
-      const payload = await this.jwtSigningService.verifyJwt(token);
+//     try {
+//       const payload = await this.jwtSigningService.verifyJwt(token);
 
-      request.tokenPayload = payload;
-      return true;
-    } catch (err) {
-      throw new UnauthorizedException('Invalid token credentials');
-    }
-  }
+//       request.tokenPayload = payload;
+//       return true;
+//     } catch (err) {
+//       throw new UnauthorizedException('Invalid token credentials');
+//     }
+//   }
 
-  private async validateRequest(request: Request): Promise<string | undefined> {
-    const token = request.headers.authorization?.split(' ')[1];
+//   private async validateRequest(request: Request): Promise<string | undefined> {
+//     const token = request.headers.authorization?.split(' ')[1];
 
-    if (!token) {
-      throw new UnauthorizedException('Invalid token');
-    }
+//     if (!token) {
+//       throw new UnauthorizedException('Invalid token');
+//     }
 
-    return token;
-  }
-}
+//     return token;
+//   }
+// }
 
 export function IsAuthorizedUser() {
   return UseGuards(AuthGuard);
