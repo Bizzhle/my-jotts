14b2cf163c283fbf0aebb22dc5a87580bbdf482f
Paul Egbo <gabrieluche2@gmail.com>
2025-10-19 19:40:35 +0200

    update datasource

--- portal-backend/sql/data-source.ts
+++ portal-backend/sql/data-source.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { config as dotenvConfig } from 'dotenv';
 import { join, resolve } from 'path';
 import 'reflect-metadata';
-import { Account } from 'src/users/entities/Account.entity';
-import { Session } from 'src/users/entities/Session.entity';
-import { User } from 'src/users/entities/User.entity';
-import { Verification } from 'src/users/entities/Verification';
+import 'tsconfig-paths/register';
 import { DataSource } from 'typeorm';
 import { EnvironmentConfigRootModule } from '../src/app/configuration/Environment';
 import { TypeOrmRootModule } from '../src/app/configuration/TypeORM';
+import { Account } from '../src/users/entities/Account.entity';
+import { Session } from '../src/users/entities/Session.entity';
+import { User } from '../src/users/entities/User.entity';
+import { Verification } from '../src/users/entities/Verification';
 
 @Module({
   imports: [EnvironmentConfigRootModule(), TypeOrmRootModule(true)],
@@ -31,7 +32,7 @@ export const AppDataSource = new DataSource({
   logging: process.env.DATABASE_LOG_QUERIES === 'true',
   connectTimeoutMS: 60000,
   // namingStrategy: new CustomNamingStrategy(),
-  entities: [User, Account, Verification, Session, join(__dirname, '**', '*.entity.{ts,js}')],
-  migrations: [join(__dirname, 'sql/db_migrations/**/*{.ts,.js}')],
+  entities: [User, Account, Verification, Session, join(__dirname, '..', '**', '*.entity.{ts,js}')],
+  migrations: [join(__dirname, './db_migrations/**/*{.ts,.js}')],
   ssl: process.env.DATABASE_USE_SSL === 'true',
 });
