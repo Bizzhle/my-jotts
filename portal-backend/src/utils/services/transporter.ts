2c7323cf82835a3b0626f52f65da27748f3b44ec
Paul Egbo <gabrieluche2@gmail.com>
2025-10-27 12:02:15 +0100

    backend: add email templates for subscription

---
+++ portal-backend/src/utils/services/transporter.ts
@@ -0,0 +1,21 @@
+import * as nodemailer from 'nodemailer';
+
+export const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: +process.env.SMTP_PORT,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
+
+export async function sendEmail(email: string, subject: string, html: string) {
+  const mailOptions = {
+    from: 'MyJotts',
+    to: email,
+    subject,
+    html,
+  };
+
+  return transporter.sendMail(mailOptions);
+}
