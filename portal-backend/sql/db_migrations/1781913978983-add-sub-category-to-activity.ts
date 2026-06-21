b82740767b9b6fd65a1cfbff13dd2c7e0b4da794
Paul Egbo <gabrieluche2@gmail.com>
2026-06-21 17:05:44 +0200

    migrate existing activity

--- portal-backend/sql/db_migrations/1781913978983-add-sub-category-to-activity.ts
+++ portal-backend/sql/db_migrations/1781913978983-add-sub-category-to-activity.ts
@@ -8,14 +8,6 @@ export class AddSubCategoryToActivity1781913978983 implements MigrationInterface
     await queryRunner.query(
       `ALTER TABLE "activity" ADD CONSTRAINT "FK_59430f4696c23ad1ad269af0518" FOREIGN KEY ("sub_category_id") REFERENCES "category"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
     );
-
-    // Update sub_category_id only if the record exists
-    const result = await queryRunner.query(`
-        SELECT id FROM activity WHERE category_id = 1 AND id = 1 LIMIT 1`);
-    if (result.length > 0) {
-      await queryRunner.query(`
-        UPDATE activity SET sub_category_id = 2 WHERE category_id = 1 AND id = 1`);
-    }
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
